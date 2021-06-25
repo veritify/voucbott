2539efb217bd423d20a30be15602ad374cfcc789
veritify <84104203+veritify@users.noreply.github.com>
2021-06-25 22:30:33 +0100

    all

---
+++ Vouch-Bot-main/Commands/help.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js')
+module.exports = {
+    name:"help",
+    aliases: ["helps", "hel", "Help", "HELP"],
+    description:"A simple ping pong command",
+    aliases: [],
+    execute: async function(message, args, client, user) {
+        const embed = new Discord.MessageEmbed()
+        .setTitle("**Commands**")
+        .setDescription("```+ping - Sends Pong! \n +vouch (user) (reason) - Vouches mentioned person \n +downvote (user) (reason) - Downvotes mentioned user.\n+reputation (user) - Sends amount of downvotes and vouches they have. ```")
+        .setTimestamp()
+        message.channel.send(embed)
+    }
+}
---
+++ Vouch-Bot-main/Commands/ping.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js')
+module.exports = {
+    name:"ping",
+    description:"A simple ping pong command",
+    aliases: [],
+    execute: async function(message, args, client, user) {
+        message.channel.send("pong!")
+    }
+}
