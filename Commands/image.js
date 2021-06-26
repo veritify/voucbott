7e2a63cae85c0811b5b4c3540705bfb3bab82fac
veritify <84104203+veritify@users.noreply.github.com>
2021-06-26 18:02:35 +0100

    image add

---
+++ Commands/image.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js')
+var Scraper = require('images-scraper');
+module.exports = {
+    name: "image",
+    execute: async function(message, args, client) {
+        const google = new Scraper({
+            puppeteer: {
+                headless = true,
+            }
+        })
+        const image_query = args.join(' ')
+        if(!image_query) return message.channel.send("Please enter an image name!");
+
+        const image_result = await google.scrape(image_query, 1);
+        message.channel.send(image_result[0].url);
+    }
+}
