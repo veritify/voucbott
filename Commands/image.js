b94e5dbdef77344d07c3ce620938706c8007a44e
veritify <84104203+veritify@users.noreply.github.com>
2021-06-26 18:13:35 +0100

    123

--- Commands/image.js
+++ Commands/image.js
@@ -3,15 +3,7 @@ var Scraper = require('images-scraper');
 module.exports = {
     name: "image",
     execute: async function(message, args, client) {
-        const google = new Scraper({
-            puppeteer: {
-                headless: true,
-            }
-        })
-        const image_query = args.join(' ')
-        if(!image_query) return message.channel.send("Please enter an image name!");
-
-        const image_result = await google.scrape(image_query, 1);
-        message.channel.send(image_result[0].url);
+        const query = args.join(' ')
+        console.log(await  image_finder.find(query))
     }
 }
