291ef7eb7fa298a952e2e4364ccedc21834bac7f
veritify <84104203+veritify@users.noreply.github.com>
2021-06-26 21:08:05 +0100

    123

--- Commands/image.js
+++ Commands/image.js
@@ -5,11 +5,13 @@ module.exports = {
     execute: async function(message, args, client) {
         message.delete();
         const query = args.join(' ')
-        if(message.content.includes("porn")){
+        if(message.content.includes("porn" || "dick" || "pussy" || "vagina" || "std" || "sex" || "nigger" || "nigga")){
             message.channel.send("-_-")
-        }
-        const im = await  image_finder.find(query)
+            return
+        }else{
+        const im = await  image_finder.find(query, {size: "large"})
         message.reply("Here is your image!").then(msg => msg.delete({timeout: 12000}))
         message.channel.send(im).then(msg => msg.delete({timeout: 12000}))
+        }
     }
 }
