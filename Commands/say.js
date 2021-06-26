a15e6b9fa8a943fbc847f3c7eb2b51e95da49558
veritify <84104203+veritify@users.noreply.github.com>
2021-06-26 17:36:39 +0100

    123

--- Commands/say.js
+++ Commands/say.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 module.exports = {
     name:"say",
     execute: async function(message, args, client, user) {
-        const message = args.join(" ")
-        message.channel.send(message)
+        const wad = args.join(" ")
+        message.channel.send(wad)
     }
 }
