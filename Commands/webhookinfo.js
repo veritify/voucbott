f1a95475b64b13a42d16d23733f0ba069972082b
veritify <84104203+veritify@users.noreply.github.com>
2021-06-26 17:26:59 +0100

    123

--- Commands/webhookinfo.js
+++ Commands/webhookinfo.js
@@ -3,7 +3,7 @@ module.exports = {
     name:"webhookinfo",
     description:"A simple ping pong command",
     execute: async function(message, args, client, user) {
-        const webhook = args[0]
+        const webhook = args.join(' ')
         if(!webhook){
             const embed = new Discord.MessageEmbed()
             .setTitle("No webhook!")
