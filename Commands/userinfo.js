c4eb29982b2811ebb9b2e74d9f2f6162109fee9e
veritify <84104203+veritify@users.noreply.github.com>
2021-06-26 15:53:37 +0100

    123

--- Commands/userinfo.js
+++ Commands/userinfo.js
@@ -5,7 +5,7 @@ module.exports = {
     description:"A simple ping pong command",
     aliases: [],
     execute: async function(client,message, args) {
-        let user = message.mentions.member.first() || message.guild.member.cache.get(args[0]) || message.member;
+        let user = message.mentions.users.first() || (args[0] ? await client.users.fetch(args[0]) : undefined) || message.author
 
         let status;
         switch(user.presence.status){
