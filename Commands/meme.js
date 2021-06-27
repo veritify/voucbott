d7f376cb43593323b23ae352f3557f50fac3d800
veritify <84104203+veritify@users.noreply.github.com>
2021-06-27 09:04:02 +0100

    meme

--- Commands/meme.js
+++ Commands/meme.js
@@ -3,18 +3,22 @@ const fetch = require('node-fetch')
 module.exports = {
     name:"meme",
     execute: async function(message, args, client, user) {
-        const {f} = await fetch('https://meme-api.herokuapp.com/gimme')
-        const link = f.url
-        const subr = f.subreddit
-        const title = f.title
-        const nsfw = f.nsfw
-        const spoiler = f.spoiler
-        const upvotes = f.ups
-
-        const embed = new Discord.MessageEmbed()
-        .setTitle(title)
-        .setImage(link)
-        .setFooter(`Subreddit: ${subr} | NSFW: ${nsfw} | Spoiler: ${spoiler} | Upvotes: ${upvotes}`)
-        message.channel.send(embed)
+        fetch('https://meme-api.herokuapp.com/gimme')
+        .then(res => res.json())
+        .then(json => {
+            const embed = new Discord.MessageEmbed()
+            .setTitle(json.title)
+            .setImage(json.url)
+            .setColor("BLUE")
+            .setFooter(`Subreddit: ${json.subreddit} | Link: ${json.postLink}`);
+            let msg = await message.channel.send(
+                new Discord.MessageEmbed()
+                .setTitle("Fetching...")
+                .setDescription("Fetching you a meme!")
+                .setColor("BLUE")
+                .setTimestamp()
+            )
+            msg.edit(embed)
+        })
     }
 }
