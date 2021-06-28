d4add31351cec2456676004710323e8379af2979
veritify <84104203+veritify@users.noreply.github.com>
2021-06-28 20:43:35 +0100

    123

---
+++ Commands/trivia.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js')
+const a = require('axios')
+module.exports = {
+    name:"trivia",
+    description:"A simple ping pong command",
+    aliases: [],
+    execute: async function(message, args, client, user) {
+        f = await a.get("https://opentdb.com/api.php?amount=1&type=multiple")
+        message.channel.send(f.text)
+    }
+}
