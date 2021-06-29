419871ea0c2fba8dab2f9ba13da0040bfed52a08
veritify <84104203+veritify@users.noreply.github.com>
2021-06-29 21:01:54 +0100

    fix

--- Commands/reverse.js
+++ Commands/reverse.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js')
 const axios = require("axios")
 module.exports = {
-    name:"revers",
+    name:"reverse",
     execute: async function(message, args, client, user) {
-        msg.channel.send(args.join(' ').split('').reverse().join(''));
+        message.channel.send(args.join(' ').split('').reverse().join(''));
     }
 }
