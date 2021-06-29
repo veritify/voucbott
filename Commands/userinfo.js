e7ee3ca46c79915669b6fdf49f308b062424afcd
veritify <84104203+veritify@users.noreply.github.com>
2021-06-29 20:34:48 +0100

    s

---
+++ Commands/userinfo.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js')
+const moment = require('moment');
+module.exports = {
+    name: "downvote",
+    description: "Downvote a user",
+    aliases: [],
+    execute: async function (message, args, client, user) {
+    let user = message.mentions.users.first() || message.author;
+    const joinDiscord = moment(user.createdAt).format('llll');
+    const joinServer = moment(user.joinedAt).format('llll');
+    let embed = new Discord.RichEmbed()
+        .setAuthor(user.username + '#' + user.discriminator, user.displayAvatarURL)
+        .setDescription(`${user}`)
+        .setColor(`BLUE`)
+        .setThumbnail(`${user.displayAvatarURL}`)
+        .addField('Joined at:', `${moment.utc(user.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
+        .addField('Status:', user.presence.status, true)
+        .addField('Roles:', user.roles.map(r => `${r}`).join(' | '), true)
+        .setFooter(`ID: ${user.id}`)
+        .setTimestamp();
+
+    message.channel.send({ embed: embed });
+    return;
+    }
+}
