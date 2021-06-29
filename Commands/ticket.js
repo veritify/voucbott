da20d143f2506359a82323cbbf31539418fd8a63
veritify <84104203+veritify@users.noreply.github.com>
2021-06-29 18:25:15 +0100

    awd

--- Commands/ticket.js
+++ Commands/ticket.js
@@ -13,40 +13,10 @@ module.exports = {
             VIEW_CHANNEL: true,
             SEND_IMAGE: true,
         });
-        const reactionMessage = await channel.send("Thank you for contacting support!");
-        try {
-            await reactionMessage.react("🔒");
-            await reactionMessage.react("⛔");
-          } catch (err) {
-            channel.send("Error sending emojis!");
-            throw err;
-          }
-
-          const collector = reactionMessage.createReactionCollector(
-            (reaction, user) => message.guild.members.cache.find((member) => member.id === user.id).hasPermission("ADMINISTRATOR"),
-            { dispose: true }
-          );
-      
-          collector.on("collect", (reaction, user) => {
-            switch (reaction.emoji.name) {
-              case "🔒":
-                channel.updateOverwrite(message.author, { SEND_MESSAGES: false });
-                break;
-              case "⛔":
-                channel.send("Deleting this channel in 5 seconds!");
-                setTimeout(() => channel.delete(), 5000);
-                break;
-            }
-          });
-      
-          message.channel
-            .send(`We will be right with you! ${channel}`)
-            .then((msg) => {
-              setTimeout(() => msg.delete(), 7000);
-              setTimeout(() => message.delete(), 3000);
-            })
-            .catch((err) => {
-              throw err;
-            });
+        await channel.send(
+          new Discord.MessageEmbed()
+          .setTitle("Ticket Created!")
+          .setDescription("")
+        )
     }
-}
+}//123
